ac25741e080c1d8a059dbe10856d6e0316cec345
Paolo Caviedes <paolo.caviedes@gmail.com>
2017-06-19 01:15:06 -0400

    Modelo finalizado

--- car_analytics/main/admin.py
+++ car_analytics/main/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Transmision,Categoria,Tipo
+from .models import Transmision,Categoria,Carroceria,Pais,Region,Ciudad,Marca,Modelo,Version,Tipo,Particular,Automotora,Contacto,Vehiculo,ContactoVehiculo,TipoCambio,Edicion
 
 class TransmisionAdmin(admin.ModelAdmin):
 	list_display = ('id','tipoTransmision','sigla')
@@ -10,11 +10,67 @@ class TransmisionAdmin(admin.ModelAdmin):
 class CategoriaAdmin(admin.ModelAdmin):
 	list_display = ('id','nombreCategoria','sigla')
 
+class CarroceriaAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreCarroceria','sigla')
+
+class PaisAdmin(admin.ModelAdmin):
+	list_display = ('id','nombrePais','sigla')
+
+class RegionAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreRegion','numero','sigla','pais')
+
+class CiudadAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreCiudad','sigla','region')
+
+class MarcaAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreMarca','paisDeOrigen','sigla')
+
+class ModeloAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreModelo','sigla','marca')
+
+class VersionAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreVersion','sigla','descripcion','modelo')
+
 class TipoAdmin(admin.ModelAdmin):
 	list_display = ('id','nombreTipo','sigla','esAutomotora','esParticular')
 
+class ParticularAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreParticular','personalIdentifier','direccion','telefono','email','tipo','ciudad')
+
+class AutomotoraAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreAutomotora','direccion','comuna','telefono','email','tipo','ciudad')
+
+class ContactoAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreContacto','direccion','comuna','telefono','email','tipo','ciudad','automotora')
+
+class VehiculoAdmin(admin.ModelAdmin):
+	list_display = ('id','kilometraje','precio','anno','puertas','cilindrada','colorExterior','combustible','direccion','esNuevo','aireAcondicionado','alzavidriosElectricos','espejosElectricos','frenosAbs','airbag','unicoDuenno','cierreCentralizado','convertidorCatalitico','llantas','radio','vendido','fechadePublicacion','version','modelo','marca','tipo','ciudad','carroceria','categoria','transmision')	
+
+class ContactoVehiculoAdmin(admin.ModelAdmin):
+	list_display = ('id','vehiculo','contacto')
+
+class TipoCambioAdmin(admin.ModelAdmin):
+	list_display = ('id','nombreTipoCambio','sigla')
+
+class EdicionAdmin(admin.ModelAdmin):
+	list_display = ('id','fechadePublicacion','valorAnterior','valorNuevo','campoEditado','vehiculo','tipoCambio')
+
 admin.site.register(Transmision,TransmisionAdmin)
 admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Carroceria,CarroceriaAdmin)
+admin.site.register(Pais,PaisAdmin)
+admin.site.register(Region,RegionAdmin)
+admin.site.register(Ciudad,CiudadAdmin)
+admin.site.register(Marca,MarcaAdmin)
+admin.site.register(Modelo,ModeloAdmin)
+admin.site.register(Version,VersionAdmin)
 admin.site.register(Tipo,TipoAdmin)
+admin.site.register(Particular,ParticularAdmin)
+admin.site.register(Automotora,AutomotoraAdmin)
+admin.site.register(Contacto,ContactoAdmin)
+admin.site.register(Vehiculo,VehiculoAdmin)
+admin.site.register(ContactoVehiculo,ContactoVehiculoAdmin)
+admin.site.register(TipoCambio,TipoCambioAdmin)
+admin.site.register(Edicion,EdicionAdmin)
 
 # Register your models here.
--- car_analytics/main/models.py
+++ car_analytics/main/models.py
@@ -13,12 +13,118 @@ class Categoria(models.Model):
 	nombreCategoria = models.CharField(max_length=100)
 	sigla=models.CharField(max_length=10)
 
+class Carroceria(models.Model):
+	nombreCarroceria = models.CharField(max_length=100)
+	sigla=models.CharField(max_length=10)
+
+class Pais(models.Model):
+	nombrePais = models.CharField(max_length=100)
+	sigla=models.CharField(max_length=10)
+
+class Region(models.Model):
+	nombreRegion = models.CharField(max_length=100)
+	numero = models.IntegerField()
+	sigla=models.CharField(max_length=10)
+	pais = models.ForeignKey(Pais)
+
+class Ciudad(models.Model):
+	nombreCiudad = models.CharField(max_length=100)
+	sigla=models.CharField(max_length=10)
+	region = models.ForeignKey(Region)
+
+class Marca(models.Model):
+	nombreMarca = models.CharField(max_length=100)
+	paisDeOrigen = models.ForeignKey(Pais)
+	sigla=models.CharField(max_length=10)
+
+class Modelo(models.Model):
+	nombreModelo = models.CharField(max_length=100)
+	sigla=models.CharField(max_length=10)
+	marca = models.ForeignKey(Marca)
+
+class Version(models.Model):
+	nombreVersion = models.CharField(max_length=100)
+	sigla=models.CharField(max_length=10)
+	descripcion = models.CharField(max_length=250)
+	modelo = models.ForeignKey(Modelo)
+
 class Tipo(models.Model):
 	nombreTipo = models.CharField(max_length=100)
 	sigla=models.CharField(max_length=10)
 	esParticular=models.BooleanField()
 	esAutomotora=models.BooleanField()
 
-class Carroceria(models.Model):
-	nombreCarroceria = models.CharField(max_length=100)
+class Particular(models.Model):
+	nombreParticular = models.CharField(max_length=100)
+	personalIdentifier = models.IntegerField()
+	direccion = models.CharField(max_length=200)
+	telefono = models.IntegerField()
+	email = models.CharField(max_length=100)
+	tipo = models.ForeignKey(Tipo)
+	ciudad = models.ForeignKey(Ciudad)
+
+class Automotora(models.Model):
+	nombreAutomotora = models.CharField(max_length=100)
+	direccion = models.CharField(max_length=200)
+	comuna = models.CharField(max_length=200)
+	telefono = models.IntegerField()
+	email = models.CharField(max_length=100)
+	tipo = models.ForeignKey(Tipo)
+	ciudad = models.ForeignKey(Ciudad)
+
+class Contacto(models.Model):
+	nombreContacto = models.CharField(max_length=100)
+	direccion = models.CharField(max_length=200)
+	comuna = models.CharField(max_length=200)
+	telefono = models.IntegerField()
+	email = models.CharField(max_length=100)
+	tipo = models.ForeignKey(Tipo)
+	ciudad = models.ForeignKey(Ciudad)
+	automotora = models.ForeignKey(Automotora)
+
+class Vehiculo(models.Model):
+	kilometraje = models.IntegerField()
+	precio = models.IntegerField()
+	anno = models.IntegerField()
+	puertas = models.IntegerField()
+	cilindrada = models.IntegerField()
+	colorExterior = models.CharField(max_length=100)
+	combustible = models.CharField(max_length=100)
+	direccion = models.CharField(max_length=100)
+	esNuevo = models.BooleanField()
+	aireAcondicionado = models.BooleanField()
+	alzavidriosElectricos = models.BooleanField()
+	espejosElectricos = models.BooleanField()
+	frenosAbs = models.BooleanField()
+	airbag = models.BooleanField()
+	unicoDuenno = models.BooleanField()
+	cierreCentralizado = models.BooleanField()
+	convertidorCatalitico = models.BooleanField()
+	llantas = models.BooleanField()
+	radio = models.BooleanField()
+	vendido = models.BooleanField()
+	fechadePublicacion = models.DateTimeField(auto_now=True, auto_now_add=False)
+	version = models.ForeignKey(Version)
+	modelo = models.ForeignKey(Modelo)
+	marca = models.ForeignKey(Marca)
+	tipo = models.ForeignKey(Tipo)
+	ciudad = models.ForeignKey(Ciudad)
+	carroceria = models.ForeignKey(Carroceria)
+	categoria = models.ForeignKey(Categoria)
+	transmision = models.ForeignKey(Transmision)
+
+class ContactoVehiculo(models.Model):
+	vehiculo = models.ForeignKey(Vehiculo)
+	contacto = models.ForeignKey(Contacto)
+
+class TipoCambio(models.Model):
+	nombreTipoCambio = models.CharField(max_length=100)
 	sigla=models.CharField(max_length=10)
+
+class Edicion(models.Model):
+	fechadePublicacion = models.DateTimeField(auto_now=False, auto_now_add=False)
+	valorAnterior = models.CharField(max_length=200)
+	valorNuevo = models.CharField(max_length=200)
+	campoEditado = models.CharField(max_length=200)
+	vehiculo = models.ForeignKey(Vehiculo)
+	tipoCambio = models.ForeignKey(TipoCambio)
